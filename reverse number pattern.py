f9fcecf6aee9eecfd861a535fcdbc9c3f4bc0abe
raihana <raihanaummu@gmail.com>
2022-08-24 16:10:13 +0800

    commit

---
+++ reverse number pattern.py
@@ -0,0 +1,6 @@
+row=5
+number=5
+for i in range (0,number+1):
+    for j in range (row-i,0,-1):
+        print(j, end=' ')
+    print()
---
+++ while loop.py
@@ -0,0 +1,6 @@
+counter=0
+number=2412
+while number!=0:
+    number=number//10
+    counter+=1
+print (counter)
