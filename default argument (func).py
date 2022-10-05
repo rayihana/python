10bd660beb94bf1e4b5f3d134793385bc459d79d
raihana <raihanaummu@gmail.com>
2022-10-05 21:21:47 +0800

    commit

---
+++ default argument (func).py
@@ -0,0 +1,5 @@
+def show_employee (name, salary=9000):
+    print("Name: ", name, " salary:", salary)
+
+show_employee("ben", 12000)
+show_employee("jessa")
---
+++ return multiple values (func).py
@@ -0,0 +1,8 @@
+def formula (a,b):
+    minus=a-b
+    add=a+b
+
+    return minus,add
+
+res=formula(40,10)
+print(res)
